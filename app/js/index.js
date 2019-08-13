f531b8b1151a786a939212dd3dcfc0e7893c8f7c
Vladimir <shikarno_3000@inbox.ru>
2019-08-13 22:33:13 +0300

    all done

--- app/js/index.js
+++ app/js/index.js
@@ -2,9 +2,17 @@ const navSlide = () => {
   const burger = document.querySelector('.burger-wrapper'),
         nav = document.querySelector('.nav')
 
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active')
-  })
+  burger.addEventListener('click', () => nav.classList.toggle('nav-active'))
 }
 
 navSlide()
+
+// const selectedLink = () => {
+//   const navLink = document.querySelector('.nav__link')
+
+//   navLink.addEventListener('click', () => {
+//     if (!navLink.classList.contains('selected')) console.log('1')
+//   })
+// }
+
+// selectedLink()
